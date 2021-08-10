4265f416bc0277815150688abaf39f0a9e4434c9
naijun0403 <bluerex@mcna.xyz>
2021-08-10 15:57:36 +0900

    update 1.1.4

--- index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Created by archethic on 2021/08/06
- */
-
-export * from './src/api';
-export * from './src/client';
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by archethic on 2021/08/06
+ */
+
+export * from './api';
+export * from './client';
